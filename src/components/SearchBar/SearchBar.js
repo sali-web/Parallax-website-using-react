f7f4c54c62f9e8629ab78ad036b6d6136fb03e4e
Salina <salina.gurung009@gamil.com>
2023-11-08 16:02:30 +0545

    CHanges to Navbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -38,6 +38,70 @@ export const SearchBar = () => {
             </div>
           </div>
         </div>
+
+        <div className="info">
+          <div className="flex m-0 p-0 gap-2">
+            <img src="monument.svg" alt="" />
+            <h4 className="m-0 p-0">Bhaktapur Durbar Square</h4>
+          </div>
+          <p className="m-0 p-0">
+            Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed
+            do eiusmod tempor <br /> incididunt ut labore et dolore magna...
+          </p>
+          <div className="flex m-0 p-0 right gap-2">
+            <div className="flex m-0 p-0 gap-2" onClick={navigateToDescriptionPage}>
+              <img src="info.svg" alt="" className="m-0 p-0" />
+              <p className="m-0 p-0"> Details</p>
+            </div>
+            <div className="flex m-0 p-0 gap-2">
+              <img src="location.svg" alt="" className="m-0 p-0" />
+              <p className="m-0 p-0"> Map</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="info">
+          <div className="flex m-0 p-0 gap-2">
+            <img src="monument.svg" alt="" />
+            <h4 className="m-0 p-0">Bhaktapur Durbar Square</h4>
+          </div>
+          <p className="m-0 p-0">
+            Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed
+            do eiusmod tempor <br /> incididunt ut labore et dolore magna...
+          </p>
+          <div className="flex m-0 p-0 right gap-2">
+            <div className="flex m-0 p-0 gap-2" onClick={navigateToDescriptionPage}>
+              <img src="info.svg" alt="" className="m-0 p-0" />
+              <p className="m-0 p-0"> Details</p>
+            </div>
+            <div className="flex m-0 p-0 gap-2">
+              <img src="location.svg" alt="" className="m-0 p-0" />
+              <p className="m-0 p-0"> Map</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="info">
+          <div className="flex m-0 p-0 gap-2">
+            <img src="monument.svg" alt="" />
+            <h4 className="m-0 p-0">Bhaktapur Durbar Square</h4>
+          </div>
+          <p className="m-0 p-0">
+            Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed
+            do eiusmod tempor <br /> incididunt ut labore et dolore magna...
+          </p>
+          <div className="flex m-0 p-0 right gap-2">
+            <div className="flex m-0 p-0 gap-2" onClick={navigateToDescriptionPage}>
+              <img src="info.svg" alt="" className="m-0 p-0" />
+              <p className="m-0 p-0"> Details</p>
+            </div>
+            <div className="flex m-0 p-0 gap-2">
+              <img src="location.svg" alt="" className="m-0 p-0" />
+              <p className="m-0 p-0"> Map</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
